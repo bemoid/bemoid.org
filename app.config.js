7883d37768d2b1b424477bf3f4ab82b1ad5086f6
jedrzejchalubek <jedrzej.chalubek@gmail.com>
2020-04-06 00:49:35 +0200

    revert(): Resolve paths using __dirname var

--- app.config.js
+++ app.config.js
@@ -2,9 +2,9 @@ const path = require('path')
 
 module.exports = {
   paths: {
-    public: path.resolve(__dirname, 'public'),
-    data: path.resolve(__dirname, 'resources/data'),
-    sass: path.resolve(__dirname, 'resources/sass'),
-    bemoid: path.resolve(__dirname, 'node_modules/@bemoid/bemoid')
+    public: path.resolve('public'),
+    data: path.resolve('resources/data'),
+    sass: path.resolve('resources/sass'),
+    bemoid: path.resolve('node_modules/@bemoid/bemoid')
   }
 }
